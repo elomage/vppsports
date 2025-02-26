85208db23801ac23848835442feeb1726d5469fa
reinis-vimba <reinis.vimba@gmail.com>
2025-02-26 12:06:49 +0200

    Configured playback controls

--- src/client/client-app/src/PlaybackControl.jsx
+++ src/client/client-app/src/PlaybackControl.jsx
@@ -1,44 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './PlaybackControl.css';
 
 const PlaybackControl = ({ selectedRun, setSliderValue }) => {
-    const [sliderValue, setLocalSliderValue] = React.useState(0);
-    const [isPlaying, setIsPlaying] = React.useState(false);
+    const [sliderValue, setLocalSliderValue] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentTime, setCurrentTime] = useState(selectedRun.orientationData[0][0]);
 
     const handleInput = (event) => {
-        const value = event.target.value;
+        const value = parseInt(event.target.value, 10);
         console.log('Input event:', value);
         setLocalSliderValue(value);
         setSliderValue(value);
+        setIsPlaying(false); // Stop playback when the slider is moved manually
+        setCurrentTime(selectedRun.orientationData[value][0]);
     };
 
-    const handleClick = (event) => {
-        console.log('Click event:', event.target.id);
+    const pausePlayback = () => {
+        setIsPlaying(false);
+    };
+    
+    const startPlayback = () => {
+        console.log(sliderValue);
+        setIsPlaying(true);
     };
 
     const resetPlayback = () => {
         setLocalSliderValue(0);
         setSliderValue(0);
+        setIsPlaying(false);
+        setCurrentTime(selectedRun.orientationData[0][0]);
     };
+
+    useEffect(() => {
+        let interval;
+        if (isPlaying) {
+            interval = setInterval(() => {
+                setLocalSliderValue((prevValue) => {
+                    const newValue = Math.min(prevValue + 1, selectedRun.orientationData.length - 1);
+                    setSliderValue(newValue);
+                    setCurrentTime(selectedRun.orientationData[newValue][0]);
+                    return newValue;
+                });
+            }, 100);
+        } else {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [isPlaying, selectedRun.orientationData.length, setSliderValue]);
+
+    useEffect(() => {
+        if (!isPlaying) {
+            setLocalSliderValue(sliderValue);
+            setCurrentTime(selectedRun.orientationData[sliderValue][0]);
+        }
+    }, [sliderValue, isPlaying, selectedRun.orientationData]);
 
     return (
         <div className="playback-controls d-flex">
             <input
                 type="range"
                 min="0"
-                max={selectedRun.orientationData.length}
+                max={selectedRun.orientationData.length - 1}
                 value={sliderValue}
-                defaultValue="1"
                 className="p-2 w-100"
                 id="time-slider"
                 onInput={handleInput}
-                onClick={handleClick}
             />
             <div className="btn-group">
-                <button id="start-button" type="button" className="btn btn-success" onClick={handleClick}>
+                <button id="start-button" type="button" className="btn btn-success" onClick={startPlayback}>
                     <i className="fas fa-play"></i>
                 </button>
-                <button id="pause-button" type="button" className="btn btn-outline-success" onClick={handleClick}>
+                <button id="pause-button" type="button" className="btn btn-outline-success" onClick={pausePlayback}>
                     <i className="fas fa-pause"></i>
                 </button>
                 <button id="reset-button" type="button" className="btn btn-outline-danger" onClick={resetPlayback}>
@@ -46,7 +78,7 @@ const PlaybackControl = ({ selectedRun, setSliderValue }) => {
                 </button>
             </div>
             <h3 id="run-time">
-                {selectedRun.orientationData[0][0]}s
+                {currentTime}s
             </h3>
         </div>
     );
